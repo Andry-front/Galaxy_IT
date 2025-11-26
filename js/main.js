679ffba0b520aeb22068ea7c7627f15d32f5aafd
Andry-front <bokiyandrey5@gmail.com>
2025-11-26 14:36:00 +0200

    Added header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tab');
 
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const parentTabs = tab.closest('.tabs');
+            const allTabContents = parentTabs.querySelectorAll('.tab-content');
+            const allTabs = parentTabs.querySelectorAll('.tab');
+
+            allTabContents.forEach(content => content.classList.remove('tab-content-active'));
+            allTabs.forEach(t => t.classList.remove('tab-active'));
+
+            const targetContent = parentTabs.querySelector(`.tab-content[data-content="${tab.getAttribute('data-tab')}"]`);
+            if (targetContent) {
+                targetContent.classList.add('tab-content-active');
+
+                const nestedTab = targetContent.querySelector('.tab');
+                if (nestedTab) {
+                    nestedTab.click();
+                }
+            }
+            tab.classList.add('tab-active');
+        });
+    });
 });
